b132e6b0d55d7bf5507541855ed4c5206ae48a86
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-06-26 23:31:01 +0800

    created post route '/api/users' to create a user. Made CRUD functions async in userControllers.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,26 @@
 const { User } = require('../models');
 
+// Export controller CRUD functions
 module.exports = {
-  getUsers(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => {
-        console.error({ message: err });
-        return res.status(500).json(err);
-      });
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);  
+    } catch (err) {
+      res.status(500).json({ message: 'Your request could not be performed, please try again', body: err });
+    }
+
   },
+  
+  // Create a user
+  async createUser(req, res) {
+    try {
+      console.log(req.body)
+      const newUser = await User.create(req.body);
+      res.status(200).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: 'Your request could not be performed, please try again', body: err })
+    }
+  }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const {
   getUsers,
+  createUser
 } = require('../../controllers/userController');
 
 
 // Defining the http requests route for /api/users
 router.route('/')
   .get(getUsers)
-  .post();
+  .post(createUser);
 
 // Defining the http requests route for /api/users/:userId (with id)
 router.route('/:userId')
