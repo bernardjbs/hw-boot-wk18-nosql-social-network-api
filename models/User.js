9a58b0d82e6d6baa2b94a8e53225645bae325f60
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-06-26 23:11:57 +0800

    Implemented User model and helper function validateEmail in /utils/helpers.js to validate an email

--- models/User.js
+++ models/User.js
@@ -1,22 +1,45 @@
 const { Schema, model } = require('mongoose');
+const { validateEmail } = require('../utils/helpers');
 
-// Schema to create User model
 const userSchema = new Schema(
   {
     username: {
       type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
     email: {
       type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "invalid email"]
     },
-  },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId, 
+        ref: 'thought',
+      }
+    ], 
+    friends: [
+      {
+        type: Schema.Types.ObjectId, 
+        ref: 'user',
+      }
+    ]
+  }, 
   {
-    id: true,
+    toJSON: {
+      virtuals: true,
+    },
+    id: true, 
   }
 );
 
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
-// Initialize our User model
 const User = model('user', userSchema);
 
 module.exports = User;
